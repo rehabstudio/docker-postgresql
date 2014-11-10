d8996e1c565e04615246fff02837fc3b57f9a0ce
Patrick Carey <paddy@wackwack.co.uk>
2014-11-10 17:43:46 +0000

    Delete storage.sh

--- storage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# ensure history file exists on storage volume
-touch /var/lib/postgresql/.psql_history
-chown -R postgres:postgres /var/lib/postgresql/.psql_history
-
-echo "Storage container bootstrapped."
